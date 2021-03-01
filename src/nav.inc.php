e3e735d5f1b098588b097a389d7aa0d934b267eb
alexmi94 <alexandrecance94@gmail.com>
2021-03-01 12:44:52 +0100

    first commit

---
+++ src/nav.inc.php
@@ -0,0 +1,10 @@
+<nav>
+    <img src="./image/mail.png" alt="gmail">
+    <h1>Gmail</h1>
+    <ul>
+        <li><a href="">pour les pros</a></li>
+        <li><a href="">connexion</a></li>
+        <li><a href="">créer un compte</a></li>
+        
+    </ul>
+</nav>
